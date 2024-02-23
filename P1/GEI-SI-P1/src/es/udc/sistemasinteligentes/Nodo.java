bbf2ecfd7be4c1e73929393850c525a4dcf7db4f
gaaelgarciia <gael.garciia@udc.es>
2024-02-23 13:57:58 +0100

    Class Nodo finished

--- P1/GEI-SI-P1/src/es/udc/sistemasinteligentes/Nodo.java
+++ P1/GEI-SI-P1/src/es/udc/sistemasinteligentes/Nodo.java
@@ -1,4 +1,41 @@
 package es.udc.sistemasinteligentes;
 
 public class Nodo {
+    private Estado estado;
+    private Nodo padre;
+    private Accion accion;
+    private int coste;
+
+    public Nodo(Estado estado, Nodo padre, Accion accion, int coste) {
+        this.estado = estado;
+        this.padre = padre;
+        this.accion = accion;
+        this.coste = coste;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public Accion getAccion() {
+        return accion;
+    }
+
+    public int getCoste() {
+        return coste;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "estado=" + estado +
+                ", padre=" + padre +
+                ", accion=" + accion +
+                ", coste=" + coste +
+                '}';
+    }
 }
